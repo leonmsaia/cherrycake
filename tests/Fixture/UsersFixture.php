7764b6a7b2cc063a6fc66634d623c987a0f6e9ec
Leon M. Saia <leonmsaia@gmail.com>
2025-04-09 15:21:21 -0300

    Initial Commit

---
+++ tests/Fixture/UsersFixture.php
@@ -0,0 +1,31 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Test\Fixture;
+
+use Cake\TestSuite\Fixture\TestFixture;
+
+/**
+ * UsersFixture
+ */
+class UsersFixture extends TestFixture
+{
+    /**
+     * Init method
+     *
+     * @return void
+     */
+    public function init(): void
+    {
+        $this->records = [
+            [
+                'id' => 1,
+                'name' => 'Lorem ipsum dolor sit amet',
+                'email' => 'Lorem ipsum dolor sit amet',
+                'created' => '2025-04-09 18:06:37',
+                'modified' => '2025-04-09 18:06:37',
+            ],
+        ];
+        parent::init();
+    }
+}
